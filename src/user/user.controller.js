fbb372e89a71f61c1cb1e6cb0d09fabf0816670f
palacios <hpalacios-2020586@kinal.edu.gt>
2025-03-05 04:54:47 -0600

    quemarcion del admin

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,6 +1,33 @@
 import { hash, verify } from "argon2"
 import User from "../user/user.model.js"
 
+export const createAdmin = async () => {
+    try {
+        const admin = await User.findOne({ role: "ADMIN_ROLE" });
+
+        if (!admin) {
+            const hashedPassword = await hash("khszg&rsjUFUKE3466$sin");
+
+            const admin = new User({
+                name: "Harlin",
+                surname: "Palacios",
+                username: "hpalacios113",
+                email: "hpalacios@gmail.com",
+                password: hashedPassword,
+                phone: "21326554",
+                role: "ADMIN_ROLE",
+            });
+
+            await admin.save();
+            console.log("ADMIN_ROLE creado correctamente");
+        }else {
+            console.log("Admin ya registrado en la base")
+        }
+    } catch (error) {
+        console.error("Error al inicializar el usuario ADMIN_ROLE:", error);
+    }
+}
+
 //Buscar por id
 export const getUserById = async (req, res) => {
     try {
